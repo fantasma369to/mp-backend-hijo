fa451464d8fd07d6cb437c9e071b9a2c70964eab
fantasma369to <afiliadoudemy2019@gmail.com>
2025-07-16 11:22:40 -0500

    modificando el servidor

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor activo en http://localhost:${PORT}`);
+    console.log(`Servidor activo en mp-backend-hijo-production.up.railway.app:${PORT}`);
   })
   .on("error", (err) => {
     console.log("Error al iniciar el servidor:", err);
