16484f6aa3a93812d3e99532516824579b9031f6
fantasma369to <afiliadoudemy2019@gmail.com>
2025-07-16 10:52:37 -0500

    retirando .local.env

--- src/config/supabaseAlejandria.js
+++ src/config/supabaseAlejandria.js
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-import "dotenv/config";
 const supabaseAlejandria = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
